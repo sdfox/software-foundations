960880abc661117b48ef0c50e2d0335c5dfc8e28
sdfox <sdfox05@gmail.com>
2025-09-24 17:23:04 +0800

    add definition

--- Volume-1/basics.v
+++ Volume-1/basics.v
@@ -1,3 +1,140 @@
+From Coq Require Export String.
+
+(* Bool *)
+Inductive bool : Type :=
+  | true
+  | false.
+
+Definition negb (b:bool) : bool :=
+  match b with
+  | true => false
+  | false => true
+  end.
+Definition andb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => b2
+  | false => false
+  end.
+Definition orb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => true
+  | false => b2
+  end.
+
+Example test_orb1: (orb true false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_orb2: (orb false false) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_orb3: (orb false true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_orb4: (orb true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Notation "x && y" := (andb x y).
+Notation "x || y" := (orb x y).
+Example test_orb5: false || false || true = true.
+Proof. simpl. reflexivity. Qed.
+
+Definition negb' (b:bool) : bool :=
+  if b then false
+  else true.
+Definition andb' (b1:bool) (b2:bool) : bool :=
+  if b1 then b2
+  else false.
+Definition orb' (b1:bool) (b2:bool) : bool :=
+  if b1 then true
+  else b2.
+
+(* Numbers *)
+Module NatPlayground.
+
+Inductive nat : Type :=
+  | O
+  | S (n : nat).
+
+Definition pred (n : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => n'
+  end.
+
+End NatPlayground.
+
+Module NatPlayground2.
+
+Fixpoint plus (n : nat) (m : nat) : nat :=
+  match n with
+    | O => m
+    | S n' => S (plus n' m)
+  end.
+
+Fixpoint mult (n m : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => plus m (mult n' m)
+  end.
+Example test_mult1: (mult 3 3) = 9.
+Proof. simpl. reflexivity. Qed.
+
+Fixpoint minus (n m:nat) : nat :=
+  match n, m with
+  | O , _ => O
+  | S _ , O => n
+  | S n', S m' => minus n' m'
+  end.
+
+End NatPlayground2.
+
+ Fixpoint exp (base power : nat) : nat :=
+  match power with
+    | O => S O
+    | S p => mult base (exp base p)
+  end.
+
+Notation "x + y" := (plus x y)
+                       (at level 50, left associativity)
+                       : nat_scope.
+Notation "x - y" := (minus x y)
+                       (at level 50, left associativity)
+                       : nat_scope.
+Notation "x * y" := (mult x y)
+                       (at level 40, left associativity)
+                       : nat_scope.
+
+Fixpoint eqb (n m : nat) : bool :=
+  match n with
+  | O => match m with
+         | O => true
+         | S m' => false
+         end
+  | S n' => match m with
+            | O => false
+            | S m' => eqb n' m'
+            end
+  end.
+
+Fixpoint leb (n m : nat) : bool :=
+  match n with
+  | O => true
+  | S n' =>
+      match m with
+      | O => false
+      | S m' => leb n' m'
+      end
+  end.
+Example test_leb1: leb 2 2 = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb2: leb 2 4 = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb3: leb 4 2 = false.
+Proof. simpl. reflexivity. Qed.
+
+Notation "x =? y" := (eqb x y) (at level 70) : nat_scope.
+Notation "x <=? y" := (leb x y) (at level 70) : nat_scope.
+Example test_leb3': (4 <=? 2) = false.
+Proof. simpl. reflexivity. Qed.
+
+(**Exercises**************************************************)
 (* nandb *)
 Definition nandb (b1:bool) (b2:bool) : bool :=
   if b1 then
@@ -48,28 +185,12 @@ Example test_factorial2: (factorial 5) = (mult 10 12).
 Proof. simpl. reflexivity. Qed.
 
 (* ltb *)
-Fixpoint leb (n m : nat) : bool :=
-  match n with
-  | 0 => true
-  | S n' => match m with
-            | 0 => false
-            | S m' => leb n' m'
-            end
-  end.
-Example test_leb1: leb 2 2 = true.
-Proof. simpl. reflexivity. Qed.
-Example test_leb2: leb 2 4 = true.
-Proof. simpl. reflexivity. Qed.
-Example test_leb3: leb 4 2 = false.
-Proof. simpl. reflexivity. Qed.
-
 Definition ltb (n m : nat) : bool :=
   match m with
   | 0 => false
   | S m' => leb n m'
   end.
 
-Notation "x <? y" := (ltb x y) (at level 70) : nat_scope.
 Example test_ltb1: (ltb 2 2) = false.
 Proof. simpl. reflexivity. Qed.
 Example test_ltb2: (ltb 2 4) = true.
@@ -90,7 +211,6 @@ Proof.
 Qed.
 
 (* mult_n_1 *)
-Notation "x * y" := (mult x y).
 Theorem mult_n_1 : forall n : nat,
   n * 1 = n.
 Proof.
@@ -101,12 +221,6 @@ Proof.
 Qed.
 
 (* andb_true_elim2 *)
-Definition andb (b1:bool) (b2:bool) : bool :=
-  match b1 with
-  | true => b2
-  | false => false
-  end.
-
 Theorem andb_true_elim2 : forall b c : bool,
   andb b c = true -> c = true.
 Proof.
@@ -126,18 +240,6 @@ Proof.
 Qed.
 
 (* zero_nbeq_plus_1 *)
-Fixpoint eqb (n m : nat) : bool :=
-  match n with
-  | O => match m with
-         | O => true
-         | S m' => false
-         end
-  | S n' => match m with
-            | O => false
-            | S m' => eqb n' m'
-            end
-  end.
-Notation "x =? y" := (eqb x y) (at level 70) : nat_scope.
 Theorem zero_nbeq_plus_1 : forall n : nat,
   0 =? (n + 1) = false.
 Proof.
